8ecd6d8ccbcea60223324b0a1b58398dad4fffb4
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 12:34:50 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -5,9 +5,10 @@ import { Product } from './app.types';
 import { ProductsService } from './products.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   products: Product[];
